7e5c5bd7f8c91d70fb44b4d8993f635421c8b8e4
movo608 <moldovanandrei8399@gmail.com>
2017-11-22 16:45:35 +0200

    Commit final + footer refurbrished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react'
 import { Powered, Time, Author } from '../helpers/CustomHelpers'
-import {
-	renderEmail,
-	renderFacebook,
-	renderInstagram,
-	renderTwitter
-} from '../helpers/Icons'
 
 export default class Footer extends Component {
   	render() {
@@ -13,14 +7,14 @@ export default class Footer extends Component {
 			<footer id="footer">
 				<div className="container">
 					<ul className="icons">
-						{ renderFacebook('www.facebook.com') }
-						{ renderInstagram('www.instagram.com') }
-						{ renderTwitter('www.twitter.com') }
-						{ renderEmail('www.email.com') }
+						<li><a href="www.facebook.com" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+						<li><a href="www.instagram.com" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+						<li><a href="www.twitter.com" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+						<li><a href="www.gmail.com" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
 					</ul>
 				</div>
 				<div className="copyright">
-					<p>&copy; Asociația Generația de Azi { Time() }. All rights reserved.</p>
+					<p>&copy; <a href="">Asociația Generația de Azi</a> { Time() }. All rights reserved.</p>
 					<p>{ Author() } { Powered() }</p>
 				</div>
 			</footer>
