0a5747c20830b6e83419e6169c821fce6414c7d0
movo608 <moldovanandrei8399@gmail.com>
2017-12-04 18:59:11 +0200

    Yes
    Yes

---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,15 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'codemix/yii2-excelexport' => 
+  array (
+    'name' => 'codemix/yii2-excelexport',
+    'version' => '2.3.0.0',
+    'alias' => 
+    array (
+      '@codemix/excelexport' => $vendorDir . '/codemix/yii2-excelexport/src',
+    ),
+  ),
+);
